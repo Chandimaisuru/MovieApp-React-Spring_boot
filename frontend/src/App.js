a3930bc53e72e0ea397e2e0eff1985ae49ea8fdb
Chandimaisuru <isuruchandima92@gmail.com>
2024-07-22 10:57:25 +0530

    home routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState,useEffect } from 'react';
 import Layout from './components/Layout';
 import { Routes,Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -29,7 +30,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/'element={Layout}>
+        <Route path='/'element={<Layout/>}>
+        <Route path='/'element={<Home/>}></Route>
 
         </Route>
       </Routes>
